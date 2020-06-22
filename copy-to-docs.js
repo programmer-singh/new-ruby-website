79287fa80331b685675f5f9e18eb96df5e353f70
simarpreetsinghkalra <simarpreetsinghkalra@gmail.com>
2020-06-23 03:26:52 +0530

    added and tested content replacement script

--- copy-to-docs.js
+++ copy-to-docs.js
@@ -1,19 +1,32 @@
 // include fs-extra package
-var fs = require("fs-extra");
- 
+const fs = require("fs-extra");
+const replace = require("replace-in-file");
+
 var source = 'dist/new-ruby-website';
 var destination = 'docs/';
- fs.emptyDir('docs',err => {
-    if(err) {
+
+const replaceOpts = {
+    files: "docs/index.html",
+    from: 'base href="/"',
+    to: 'base href="./"'
+}
+
+fs.emptyDir('docs', err => {
+    if (err) {
         console.log(err);
     }
     console.log('Deleted Old Stuff');
- });
+});
+
 // copy source folder to destination
 fs.copy(source, destination, function (err) {
-    if (err){
+    if (err) {
         console.log('An error occured while copying the folder.')
         return console.error(err)
     }
     console.log('Copy completed!')
+
+    // replace content of docs/index.html after copy complete
+    const results = replace.sync(replaceOpts);
+    console.log(results);
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 
-
-import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
