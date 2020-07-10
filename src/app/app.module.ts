9f4e26d238b1ebd46b821ccde10656b2a40f1418
simarpreetsinghkalra <simarpreetsinghkalra@gmail.com>
2020-07-10 13:41:43 +0530

    'build'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { LocationStrategy, PathLocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -25,7 +25,7 @@ import { AppComponent } from './app.component';
     DrawerMenuModule,
   ],
   providers: [
-    { provide: LocationStrategy, useClass: PathLocationStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     { provide: Window, useValue: window },
   ],
   bootstrap: [AppComponent]
